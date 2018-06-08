8cd4cd5e96d4d2ddb5f4dcc248781eb16220f44c
Aivirth <aivirth.work@gmail.com>
2018-06-08 11:41:29 +0200

    Removed html-webpack-plugin as it's no longer necessary

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: ["babel-polyfill", "./src/js/index.js"],
   output: {
@@ -9,19 +8,15 @@ module.exports = {
   devServer: {
     contentBase: "./dist"
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html"
-    })
-  ],
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       }
-    }]
+    ]
   }
 };
